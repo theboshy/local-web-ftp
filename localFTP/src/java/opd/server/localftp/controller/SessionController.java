54bcf3c014c3e44e076c1d8fd55220f152b8e7d6
beatWannabe <akumaokami10@gmail.com>
2017-01-27 10:50:46 -0500

    inherit

--- localFTP/src/java/opd/server/localftp/controller/SessionController.java
+++ localFTP/src/java/opd/server/localftp/controller/SessionController.java
@@ -1,12 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package opd.server.localftp.controller;
 
 import javax.enterprise.context.RequestScoped;
-import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
 /**
