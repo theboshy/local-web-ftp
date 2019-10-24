8ec974249fd9d0c8863ebcfeeed69f46523def71
Peter Lobo <akumaokami10@gmail.com>
2019-10-24 13:24:43 -0500

    Update HashTest.java

--- localFTP/localFTP/src/test/java/org/elsamplio/localftp/test/HashTest.java
+++ localFTP/localFTP/src/test/java/org/elsamplio/localftp/test/HashTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.elsamplio.localftp.test;
 
 import org.junit.Test;
@@ -18,9 +13,6 @@ public class HashTest {
     public HashTest() {
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
     @Test
     public void hashPw() {
         System.out.println(HashPw.hashPw("Sergio25"));
